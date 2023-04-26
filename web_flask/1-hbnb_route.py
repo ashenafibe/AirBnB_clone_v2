27bdee0c255540425743b4cef3dbbee1ffeb4116
ashenafibe <111124021+ashenafibe@users.noreply.github.com>
2023-04-26 14:00:11 +0300

    Update 1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -2,15 +2,22 @@
 """class Flask"""
 from flask import Flask
 """class Flask"""
+
+
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-  return "Hello HBNB!"
+    """displays text
+    Returns:
+        text
+    """
+    return "Hello HBNB!"
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return "HBNB!"
+    return "HBNB"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
